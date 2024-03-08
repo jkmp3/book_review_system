d2b96ebb78d55ebbece2ee19fbe77972a431b147
jkmp3 <jayakrishnan.mp3@gmail.com>
2024-03-08 18:05:49 +0530

    update

--- app/enums/string_comparison.py
+++ app/enums/string_comparison.py
@@ -4,6 +4,16 @@ import enum
 
 
 class StringComparison(str, enum.Enum):
+    """
+    StringComparison defines the available comparison operators for
+    string-based filtering in queries.
+
+    Members:
+        EQUALS (str): Performs a case-sensitive exact match.
+        EQUALS_IGNORE_CASE (str): Performs a case-insensitive exact match.
+        CONTAINS (str): Performs a substring search.
+        CONTAINS_IGNORE_CASE (str): Performs a case-insensitive substring search.
+    """
     EQUALS = "equals"
     EQUALS_IGNORE_CASE = "equals_ignore_case"
     CONTAINS = "contains"
--- app/routers/books.py
+++ app/routers/books.py
@@ -1,10 +1,12 @@
-from typing import Optional
+from typing import Optional, Any
 
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
+from app.enums.error_codes import ErrorCodes
 from app.repositories import book_repository
-from app.schemas.schemas import BookSchema, FilterSchema
+from app.schemas.request_schemas import BookSchema, FilterSchema
+from app.schemas.response_schemas import ErrorResponse
 from app.utils.db_utils import get_db_session
 
 router = APIRouter()
@@ -13,7 +15,20 @@ router = APIRouter()
 @router.post("/books")
 async def add_book(book: BookSchema,
                    db: Session = Depends(get_db_session)):
-    book_repository.add_book(db, book)
+    """
+
+    Args:
+        book: book instance with the data to be saved
+        db: database session object
+
+    Returns:
+        returns the saved object, if duplicate throws error
+    """
+    try:
+        book_repository.add_book(db, book)
+    except ValueError:
+        return ErrorResponse(code=ErrorCodes.BOOK_ALREADY_EXISTS.error_id,
+                             message=ErrorCodes.BOOK_ALREADY_EXISTS.message).dict()
     return {"message": "Book added successfully."}
 
 
@@ -30,5 +45,18 @@ async def get_books(filter_schema: Optional[FilterSchema] = None,
 
 
 @router.get("/books/{book_id}")
-async def get_book_by_id(book_id: int, db: Session = Depends(get_db_session)):
-    return book_repository.get_book_by_id(db, book_id)
+async def get_book_by_id(book_id: int, db: Session = Depends(get_db_session)) -> dict[str, Any]:
+    """
+    Returns the info on book with the given id
+    Args:
+        book_id: id of the book
+        db: database session object
+
+    Returns:
+        Returns the book, or error response if book with id does not exist
+    """
+    book = book_repository.get_book_by_id(db, book_id)
+    if book is None:
+        return ErrorResponse(code=ErrorCodes.BOOK_DOES_NOT_EXIST.error_id,
+                             message=ErrorCodes.BOOK_DOES_NOT_EXIST.message).dict()
+    return book
